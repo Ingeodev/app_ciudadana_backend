8517628eca10d151c7e7086a6b4feed273cf1336
bitjep <julian.plazas@thebitbang.company>
2023-07-24 09:46:17 -0500

    [FEATURE] Added Advertisement and AdvertisementCategory models.

---
+++ src/models/advertisementcategory.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class AdvertisementCategory extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  AdvertisementCategory.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'AdvertisementCategory',
+    timestamps: true,
+    paranoid: true,
+    updatedAt: false,
+  });
+  return AdvertisementCategory;
+};
