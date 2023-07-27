5a54daf16689e5a23a4dd890ea5f7e8ceee87ad0
bitjep <julian.plazas@thebitbang.company>
2023-07-27 12:00:31 -0500

    [FEATURE] Added tests.

---
+++ src/__tests__/notifications/publicity.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+
+const localTestHost = 'http://localhost:3000/v1/notifications/publicity';
+const onlineTestHost = 'https://k7gmmdc9dj.us-east-1.awsapprunner.com/v1/notifications/publicity';
+const usedHost = localTestHost;
+
+describe("Publicity consumption API points: ", () => {
+    jest.setTimeout(8000);
+
+    const bannersItem = {
+        image: expect.any(String),
+        url: expect.any(String),
+        category: expect.any(String),
+    };
+
+    describe("GET /v1/notifications/publicity/banners ", () => {
+        test("should respond with status 200 and an array of objects with: image, url, and category.", async () => {
+            const response0 = await request(usedHost).get('/banners');
+            expect(response0.statusCode).toBe(200);
+            expect(response0.body).toEqual(expect.any(Array));
+            expect(response0.body.length).toBeGreaterThanOrEqual(0);
+            response0.body.forEach(item => {
+                expect(item).toMatchSnapshot(bannersItem);
+            });
+        });
+    });
+});
