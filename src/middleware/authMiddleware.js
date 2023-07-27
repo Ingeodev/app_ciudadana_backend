d85cf9d221a8176126fa3afadacf147426e7054b
AndresGarzonJ-TBBC <andres.garzon@thebitbang.company>
2023-07-27 10:45:03 -0500

    [Fix] API Error Format Response in authMiddleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,5 +1,6 @@
 const adminFirebase = require("firebase-admin");
 const serviceAccount = require("../account_service_key.json");
+const { StatusCodes } = require("http-status-codes");
 
 const appFirebase = adminFirebase.initializeApp({
     credential: adminFirebase.credential.cert(serviceAccount)
@@ -8,38 +9,65 @@ const authMiddleware = async (req, res, next) => {
     console.log('going to check the time and auth credentials:', Date.now())
     const { authorization } = req.headers
     if (!authorization) {
-        return res.status(401).send({message: 'Unauthorized'});
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+          status: StatusCodes.UNAUTHORIZED,
+          code: "Unauthorized",
+          detail: "Missing header Authorization",
+        });
     }
 
     if (!authorization.startsWith('Bearer')) {
-        return res.status(401).send({message: 'Unauthorized'});
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+          status: StatusCodes.UNAUTHORIZED,
+          code: "Unauthorized",
+          detail: "an authorization token starting with 'Bearer' is expected",
+        });
     }
     const split = authorization.split('Bearer ')
     if (split.length !== 2) {
-        return res.status(401).send({ message: 'Unauthorized' });
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+          status: StatusCodes.UNAUTHORIZED,
+          code: "Unauthorized",
+          detail: "authorization header is not in the expected format. It should be 'Bearer [token]'",
+        });
     }
     const token = split[1]
     try {
-        const decodedToken = await appFirebase.auth().verifyIdToken(token);
-        console.log("decodedToken", JSON.stringify(decodedToken))
-        res.locals = { ...res.locals, uid: decodedToken.uid, role: decodedToken.role }
-        next();
-    }
-    catch (err) {
-        console.error(`${err.code} -  ${err.message}`)
-        return res.status(401).send({ message: 'Unauthorized' });
+      const decodedToken = await appFirebase.auth().verifyIdToken(token);
+      console.log("decodedToken", JSON.stringify(decodedToken));
+      res.locals = {
+        ...res.locals,
+        uid: decodedToken.uid,
+        role: decodedToken.role,
+      };
+      next();
+    } catch (error) {
+      console.error(`${error.code} -  ${error.message}`);
+      // ! Uso del middleware errorMiddleware - Verificar con Julian
+      return next(error);
     }
 };
 
 const hasPermissions = (params) => {
-   return (req, res, next) => {
-       const tokenRole = res.locals.role;
-       if (params.role === tokenRole){
-           next()
-       } else {
-           return res.status(401).send({ message: 'role not valid' });
-       }
-   }
+    try {
+        return (req, res, next) => {
+          const tokenRole = res.locals.role;
+          if (params.role === tokenRole) {
+            next();
+          } else {
+            return res.status(StatusCodes.FORBIDDEN).json({
+              status: StatusCodes.FORBIDDEN,
+              code: "Forbidden",
+              detail: "role not valid",
+            });
+          }
+        };
+    } catch (error) {
+        console.error(`${error.code} -  ${error.message}`);
+        // ! Uso del middleware errorMiddleware - Verificar con Julian
+        return next(error);
+    }
+   
 };
 
 module.exports = {
