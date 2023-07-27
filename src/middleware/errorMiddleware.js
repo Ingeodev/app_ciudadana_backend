0c6882331da98cd922bc5d3874e9e88e5065c42b
bitjep <julian.plazas@thebitbang.company>
2023-07-27 16:21:25 -0500

    [FIX] Updated error middleware.

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,15 +1,21 @@
-const { StatusCodes } = require('http-status-codes');
+const { StatusCodes, getReasonPhrase } = require('http-status-codes');
 
 
 const errorHandler = (error, req, res, next) => {
-    if (!error.status) {
-        error.status = StatusCodes.INTERNAL_SERVER_ERROR;
-    }
-    return res.status(error.status).json({
-      status: StatusCodes.INTERNAL_SERVER_ERROR,
-      code: "Internal Server Error",
-      detail: error.message,
-    });
+  if (!error.status) {
+    error.status = StatusCodes.INTERNAL_SERVER_ERROR;
+  }
+  let errorCode;
+  try{
+    errorCode = getReasonPhrase(error.status);
+  } catch (err) {
+    errorCode = `Undefined error code: ${error.status}`;
+  }
+  return res.status(error.status).json({
+    status: error.status,
+    code: errorCode,
+    detail: error.message,
+  });
 };
 
 module.exports = errorHandler;
