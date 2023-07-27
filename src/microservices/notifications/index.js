d464428e9a3295e08027e01fe252ed51e1206cb5
AndresGarzonJ-TBBC <andres.garzon@thebitbang.company>
2023-07-27 16:02:55 -0500

    [Feat] Create files (driver and routes) for /attention_lines

--- src/microservices/notifications/index.js
+++ src/microservices/notifications/index.js
@@ -5,8 +5,10 @@ const bodyParser = require("body-parser");
 const { StatusCodes } = require('http-status-codes');
 
 const errorHandler = require('../../middleware/errorMiddleware');
-const advertisingRouter = require('./routes/advertisement');
-const publicityRouter = require('./routes/publicity');
+const advertisingRouter = require("./routes/advertisement");
+const publicityRouter = require("./routes/publicity");
+const attentionLinesAppMobileRouter = require("./routes/serviceHotlinesAppMobile");
+const attentionLinesAppWebRouter = require("./routes/serviceHotlinesAppWeb");
 
 const app = express();
 
@@ -19,10 +21,12 @@ app.get("/health", function (req, res) {
 
 //#region Web-oriented end-points
 app.use('/v1/notifications/advertising', advertisingRouter);
+app.use("/v1/attention_lines/mobile/", attentionLinesAppWebRouter);
 //#endregion
 
 //#region Mobile-oriented end-points
-app.use('/v1/notifications/publicity', publicityRouter);
+app.use("/v1/notifications/publicity", publicityRouter);
+app.use("/v1/attention_lines/web/", attentionLinesAppMobileRouter);
 //#endregion
 
 //#region Error handling
---
+++ src/microservices/notifications/routes/serviceHotlinesAppWeb.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+// const controller = require("../controllers/publicity");
+
+// TODO: require authentication for every point
+
+// Retrieve the advertisements that have no category attached.
+// router.get("/", controller.getUncategorized);
+
+module.exports = router;
