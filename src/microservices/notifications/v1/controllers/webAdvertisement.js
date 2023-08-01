bde3b7e378d78b3b1285c7718282ac07fe46d1ef
bitjep <julian.plazas@thebitbang.company>
2023-08-01 16:26:18 -0500

    [FEATURE] Added postAdvertisementEdit.

--- src/microservices/notifications/v1/controllers/webAdvertisement.js
+++ src/microservices/notifications/v1/controllers/webAdvertisement.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require('http-status-codes');
 const db = require('../../../../models');
 const validator = require('../../utils/validator');
+const { where } = require('sequelize');
 
 // Retrieve all the advertisements whether they have a category or not.
 const getAllAdvertisements = async (req, res, next) => {
@@ -51,8 +52,62 @@ const postAdvertisement = async (req, res, next) => {
     }
 };
 
+// Update an advertisement.
+const postAdvertisementEdit = async (req, res, next) => {
+    try {
+        const update = await validator.validateEditAdvertisementSchema(req.body);
+        const advertisement = await db.Advertisement.findByPk(update.id);
+        if (advertisement == null)
+            throw {
+                status: StatusCodes.NOT_FOUND,
+                message: `The requested Advertisement with id ${update.id} does not exist.`
+            };
+        delete update.id;
+        const updatedAdvertisement = await advertisement.update(update);
+        return res.status(StatusCodes.OK)
+            .json({ data: { ...updatedAdvertisement.dataValues, deletedAt: undefined } });
+    } catch (error) {
+        return next(error);
+    }
+};
+
+// Update the status of an advertisement.
+const postAdvertisementStatus = async (req, res, next) => {
+    try {
+        const { imageUri, siteUri, categoryId } = await validator.validateAdvertisementSchema(req.body);
+        const newAdvertisement = await db.Advertisement.create({
+            imageUri,
+            siteUri,
+            categoryId,
+        });
+        return res.status(StatusCodes.OK)
+            .json({ data: { ...newAdvertisement.dataValues, deletedAt: undefined } });
+    } catch (error) {
+        return next(error);
+    }
+};
+
+// Delete an advertisement.
+const postAdvertisementDelete = async (req, res, next) => {
+    try {
+        const { imageUri, siteUri, categoryId } = await validator.validateAdvertisementSchema(req.body);
+        const newAdvertisement = await db.Advertisement.create({
+            imageUri,
+            siteUri,
+            categoryId,
+        });
+        return res.status(StatusCodes.OK)
+            .json({ data: { ...newAdvertisement.dataValues, deletedAt: undefined } });
+    } catch (error) {
+        return next(error);
+    }
+};
+
 
 module.exports = {
     getAllAdvertisements,
     postAdvertisement,
+    postAdvertisementEdit,
+    postAdvertisementStatus,
+    postAdvertisementDelete,
 };
