c66df6bffd7ca6741383435e4a67a80d8dfc2a4f
bitjep <julian.plazas@thebitbang.company>
2023-08-01 12:06:58 -0500

    [FEATURE] Added get Advertisements List with pagination.

--- src/microservices/notifications/v1/controllers/webAdvertisement.js
+++ src/microservices/notifications/v1/controllers/webAdvertisement.js
@@ -4,8 +4,44 @@ const validator = require('../../utils/validator');
 
 // Retrieve all the advertisements whether they have a category or not.
 const getAllAdvertisements = async (req, res, next) => {
-    return res.status(StatusCodes.OK)
-        .json({ msg: 'Should retrieve all the advertisements whether they have a category or not.' });
+    try {
+        const { page: pagination } = await validator.validateSimplePaginationSchema(req.query);
+        let offset;
+        let pageSize;
+        let page = 1;
+        if (pagination != null) {
+            offset = (pagination.number - 1) * pagination.size;
+            pageSize = pagination.size;
+            page = pagination.number;
+        }
+        const pageAdvertisements = await db.Advertisement.findAndCountAll({
+            paranoid: true,
+            order: [["createdAt", "DESC"]],
+            offset,
+            limit: pageSize,
+            attributes: {
+                exclude: ["deletedAt"]
+            },
+        });
+        if (pageAdvertisements.count <= 0)
+            throw {
+                status: StatusCodes.NOT_FOUND,
+                message: 'There are no Advertisements registered in the database.',
+            };
+        if (pageSize == null)
+            pageSize = pageAdvertisements.count;
+        return res.status(StatusCodes.OK).json({
+            meta: {
+                page,
+                pageSize,
+                totalRecords: pageAdvertisements.count,
+                totalPages: Math.ceil(pageAdvertisements.count / pageSize),// TODO: Validar si lo hace el front o back
+            },
+            data: pageAdvertisements.rows,
+        });
+    } catch (error) {
+        return next(error);
+    }
 };
 
 // Create a new advertisement.
