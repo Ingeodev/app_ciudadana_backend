b402180d7cf6396071b3fd815d7e7a06ebb59e51
AndresGarzonJ-TBBC <andres.garzon@thebitbang.company>
2023-08-02 20:24:07 -0500

    [Fix] Bug in Alert Model

--- src/models/alert.js
+++ src/models/alert.js
@@ -1,6 +1,6 @@
 "use strict";
-const { Model } = require("DataTypes");
-module.exports = (DataTypes, DataTypes) => {
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
   class Alert extends Model {
     /**
      * Helper method for defining associations.
@@ -72,7 +72,7 @@ module.exports = (DataTypes, DataTypes) => {
       },
     },
     {
-      DataTypes,
+      sequelize,
       modelName: "Alert",
       tableName: "Alerts",
       schema: "public",
