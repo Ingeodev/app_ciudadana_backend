176cac77bf5df6914145d222c23b14163664fbde
bitjep <julian.plazas@thebitbang.company>
2023-08-03 09:26:12 -0500

    [FIX] Changed Advertisement relationship to Alert relationship.

--- src/models/user.js
+++ src/models/user.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasOne(models.Advertisement, {
+      User.hasOne(models.Alert, {
         foreignKey: {
           name: "sentBy",
           allowNull: false,
