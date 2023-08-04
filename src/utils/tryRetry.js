12ffedd596b7de3aa9c557185d5c451f030c2076
bitjep <julian.plazas@thebitbang.company>
2023-08-04 18:06:08 -0500

    [FEATURE] Added function documentation and splitted validation.

--- src/utils/tryRetry.js
+++ src/utils/tryRetry.js
@@ -1,13 +1,41 @@
-const AsyncTryRetry = async (functionToRetry, functionArgumentsObj, attempts = 2, allowedErrorMessages = null) => {
-    // TODO: Check how to handle the functionArgumentsObj.
-    if (allowedErrorMessages == null)
-        allowedErrorMessages = [];
-    const errors = [];
-    let result;
-    if (!attempts || attempts < 1 || attempts > 10) {
+const sleepNow = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
+const validate_input = (functionToRetry, functionArgumentsObj, attempts, allowedErrorMessages, sleepms) => {
+    if (functionToRetry == null || typeof functionToRetry === 'function')
+        throw new Error("Type error: <functionToRetry> must be a function.");
+    if (functionArgumentsObj == null)
+        functionArgumentsObj = [];
+    else if (!Array.isArray(functionArgumentsObj))
+        throw new Error("Type error: if defined, <functionArgumentsObj> must be an array.");
+    if (!attempts || typeof attempts !== 'number' || attempts < 1 || attempts > 10) {
         attempts = 2;
         console.warn(`<attempts> must be between 1 and 10. The value of ${attempts} is invalid; it will be set to 2.`);
     }
+    attempts = Math.round(attempts);
+    if (allowedErrorMessages == null)
+        allowedErrorMessages = [];
+    else if (!Array.isArray(allowedErrorMessages))
+        throw new Error("Type error: if defined, <allowedErrorMessages> must be an array.");
+    if (sleepms == null || typeof sleepms !== 'number' || sleepms < 0 || sleepms > 10000) {
+        sleepms = 0;
+        console.warn(`<sleepms> must be between 0 and 10000. The value of ${sleepms} is invalid; it will be set to 0.`);
+    }
+    sleepms = Math.round(sleepms);
+};
+
+/**
+ * Asyncronously tries the function `functionToRetry` and and retries it `attempts` times if it fails.
+ * @param {Function} functionToRetry The function that will be retried if fails.
+ * @param {Array} functionArgumentsObj An Array with the arguments that are received by `functionToRetry`.
+ * @param {number} attempts Maximum number of retries; the minimum allowed is 2 and the maximum is 10.
+ * @param {string[]} allowedErrorMessages Optional. An Array of error messages that will trigger the retry of `functionToRetry`. If it is defined, only exact matches will trigger the retry. If it is not defined, any error will trigger the retry.
+ * @param {number} sleepms Optional. Milliseconds to sleep before each retry. Minimum is 0, maximum is 10000. Default is 0.
+ * @returns An object possibly including two keys: {`result`: the result from a successful try, `errors`: the thrown errors during the retries.}.
+ */
+const asyncTryRetry = async (functionToRetry, functionArgumentsObj = [], attempts = 2, allowedErrorMessages = [], sleepms = 0) => {
+    validate_input(functionToRetry, functionArgumentsObj, attempts, allowedErrorMessages);
+    const errors = [];
+    let result;
     for (let i = 0; i < attempts; i++) {
         try {
             result = await functionToRetry(...functionArgumentsObj);
@@ -18,7 +46,11 @@ const AsyncTryRetry = async (functionToRetry, functionArgumentsObj, attempts = 2
                 return { errors };
             }
         }
+        if (sleepms)
+            await sleepNow(sleepms);
     }
 };
 
-module.exports = AsyncTryRetry;
+module.exports = {
+    asyncTryRetry,
+};
