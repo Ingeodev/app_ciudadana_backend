26e77a8e5d74130a261063cc0cf66b4f58b71962
AndresGarzonJ-TBBC <andres.garzon@thebitbang.company>
2023-08-07 17:00:06 -0500

    [Feat] Add web documentType controller/validator

---
+++ src/microservices/users/utils/validators/documentTypes/web.js
@@ -0,0 +1,68 @@
+const { StatusCodes } = require("http-status-codes");
+const joi = require("joi");
+
+// const uri_string = joi.string().uri();
+// const integer_number = joi.number().integer();
+
+const registerSchema = joi.object({
+  code: joi.number().integer().greater(0).required(),
+  name: joi.string().trim().required().empty("").invalid(" "),
+  abbreviation: joi.string().trim().required().empty("").invalid(" "),
+});
+
+const editSchema = joi.object({
+  id: joi.number().integer().empty("").invalid(0).required(),
+  code: joi.number().integer().greater(0),
+  name: joi.string().trim().empty("").invalid(" "),
+  abbreviation: joi.string().trim().empty("").invalid(" "),
+});
+
+const getAllSchema = joi.object({
+  number: joi.number().integer().greater(0).required(),
+  size: joi.number().integer().greater(0).required(),
+});
+
+const getOneSchema = joi.object({
+  id: joi.number().required().empty("").greater(0).invalid(0),
+});
+
+const postActiveSchema = joi.object({
+  id: joi.number().required().empty("").greater(0).invalid(0),
+  active: joi.boolean().required(),
+});
+
+const use_validator_on_data = async (validator_schema, data) => {
+  try {
+    if (!validator_schema) {
+      return Promise.reject(new Error("validator_schema cannot be falsy."));
+    }
+    const validated_data = await validator_schema.validateAsync(data, {
+      convert: true,
+      abortEarly: true,
+      errors: { render: true, label: "key" },
+      stripUnknown: true,
+    });
+    return validated_data;
+  } catch (error) {
+    error.status = StatusCodes.BAD_REQUEST;
+    return Promise.reject(error);
+  }
+};
+
+module.exports = {
+  vWebPostRegister: async (inputData) => {
+    return await use_validator_on_data(registerSchema, inputData);
+  },
+  vWebPostEdit: async (inputData) => {
+    return await use_validator_on_data(editSchema, inputData);
+  },
+  vWebGetAll: async (inputData) => {
+    return await use_validator_on_data(getAllSchema, inputData);
+  },
+  vWebGetOneById: async (inputData) => {
+    return await use_validator_on_data(getOneSchema, inputData);
+  },
+  vWebPostStatus: async (inputData) => {
+    return await use_validator_on_data(postActiveSchema, inputData);
+  },
+};
---
+++ src/microservices/users/v1/controllers/documentTypes/web.js
@@ -0,0 +1,186 @@
+const { StatusCodes } = require("http-status-codes");
+const db = require("../../../../../models/index.js");
+// const firebase = require("../utils/firebaseAdmin.js");
+const { formatDate } = require("../../../../../middleware/formatDate.js");
+const validator = require("../../../utils/validators/documentTypes/web.js");
+
+/**
+ * Create a document type
+ * @param {object} req - Object containing the code, name, abbreviation
+ * @return {object} Response contains: statuscode (integer), json (objeto): echo reply, if 200OK. Or if there's error, json (objeto): status, code, detail
+ */
+exports.postRegister = async (req, res, next) => {
+  try {
+    const { code, name, abbreviation } = await validator.vWebPostRegister(
+      req.body
+    );
+
+    const dataQuery = {
+      code,
+      name,
+      abbreviation,
+      active: true,
+    };
+
+    const result = await db.DocumentType.create(dataQuery);
+    return res.status(StatusCodes.OK).json({ meta: null, data: result });
+  } catch (error) {
+    console.error("document type could not be created: ", error.message);
+    return next(error);
+  }
+};
+
+/**
+ * Update document type
+ * @param {object} req - Object containing the code, name, abbreviation
+ * @return {object} Response contains: statuscode (integer), json (objeto): echo reply, if 200OK. Or if there's error, json (objeto): status, code, detail
+ */
+exports.postEdit = async (req, res, next) => {
+  try {
+    const { id, code, name, abbreviation } = await validator.vWebPostEdit(req.body);
+
+    const dataQuery = {
+      id,
+      code,
+      name,
+      abbreviation,
+    };
+
+    const docTypeInDb = await db.DocumentType.findByPk(id);
+
+    if (docTypeInDb === null) {
+      throw {
+        status: StatusCodes.NOT_FOUND,
+        message: `The document type with id=${id} does not exist`,
+      };
+    }
+
+    const resultUpdate = await docTypeInDb.update(dataQuery);
+
+    return res.status(StatusCodes.OK).json({
+      meta: null,
+      data: resultUpdate,
+    });
+  } catch (error) {
+    console.error("document type could not be updated: ", error.message);
+    if (
+      error &&
+      error.errors &&
+      error.errors.length > 0 &&
+      error.errors[0].message
+    ) {
+      error.message = error.errors[0].message;
+    }
+    return next(error);
+  }
+};
+
+
+/**
+ * Get all document types
+ * @return {object} Response contains: statuscode (integer), json (objeto): data document types. Or if there's error, json (objeto): status, code, detail
+ */
+exports.getAll = async (req, res, next) => {
+  try {
+    const objPage = await validator.vWebGetAll({
+      number: req.query.page ? parseInt(req.query.page.number) : null,
+      size: req.query.page ? parseInt(req.query.page.size) : null,
+    });
+
+    const docTypesInDb = await db.DocumentType.findAndCountAll({
+      limit: objPage.size,
+      offset: (objPage.number - 1) * objPage.size,
+      order: [["createdAt", "DESC"]], // Sort by date of creation in descending order
+    });
+
+    if (docTypesInDb.count === 0) {
+      throw {
+        status: StatusCodes.NOT_FOUND,
+        message: "The requested page does not exist",
+      };
+    }
+    const totalPages = Math.ceil(docTypesInDb.count / objPage.size);
+
+    const responseCustom = {
+      meta: {
+        page: objPage.number,
+        pageSize: objPage.size,
+        totalRecords: docTypesInDb.count,
+        totalPages: totalPages,
+      },
+      data: docTypesInDb.rows,
+    };
+
+    return res.status(StatusCodes.OK).send(responseCustom);
+  } catch (error) {
+    console.error("document types could not be recovered: ", error.message);
+    return next(error);
+  }
+};
+
+/**
+ * Get document type by id
+ * @return {object} Response contains: statuscode (integer), json (objeto): data document type. Or if there's error, json (objeto): status, code, detail
+ */
+exports.getOneById = async (req, res, next) => {
+  try {
+    const { id } = await validator.vWebGetOneById({
+      id: parseInt(req.params.id),
+    });
+
+    const docTypeInDb = await db.DocumentType.findByPk(id);
+
+    if (docTypeInDb === null) {
+      throw {
+        status: StatusCodes.NOT_FOUND,
+        message: "Document type information could not be retrieved",
+      };
+    }
+
+    return res
+      .status(StatusCodes.OK)
+      .send({ meta: null, data: docTypeInDb });
+  } catch (error) {
+    console.error("Document Type could not be recovered: ", error.message);
+    return next(error);
+  }
+};
+
+/**
+ * Update the status of the documentType.active field (enabled/disabled) for a document type
+ * @return {object} Response contains: statuscode (integer), json (objeto): data document type. Or if there's error, json (objeto): status, code, detail
+ */
+exports.postStatus = async (req, res, next) => {
+  try {
+    const { id, active } = await validator.vWebPostStatus(req.body);
+    let dataQuery = {};
+    if (active === false) {
+      dataQuery = {
+        active,
+        deleteAt: formatDate(new Date()),
+      };
+    }
+    dataQuery = {
+      active,
+      updatedAt: formatDate(new Date()),
+    };
+
+    const docTypeInDb = await db.DocumentType.findByPk(id);
+
+    if (docTypeInDb === null) {
+      throw {
+        status: StatusCodes.NOT_FOUND,
+        message: `The document type with id=${id} does not exist`,
+      };
+    }
+
+    await docTypeInDb.update(dataQuery);
+
+    return res
+      .status(StatusCodes.OK)
+      .send({ meta: null, data: { id, active } });
+  } catch (error) {
+    console.error("document type could not be updated: ", error.message);
+    return next(error);
+  }
+};
