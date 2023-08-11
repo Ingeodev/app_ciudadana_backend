809d7ad957e0907e637e30e849292cfe9f958072
AndresGarzonJ-TBBC <andres.garzon@thebitbang.company>
2023-08-11 13:22:55 -0500

    [Fix] mobileSecurity: GET: /security endpoints

--- src/microservices/notifications/v1/controllers/mobileSecurity.js
+++ src/microservices/notifications/v1/controllers/mobileSecurity.js
@@ -5,40 +5,77 @@ const { formatDate } = require("../../../../middleware/formatDate.js");
 const validator = require("../../utils/validatorSecurity.js");
 
 /**
- * Get all attention lines
+ * Get all  attention lines of security/emergency
  * @return {object} Response contains: statuscode (integer), json (objeto): data attention lines. Or if there's error, json (objeto): status, code, detail
  */
 exports.getListAll = async (req, res, next) => {
   try {
-    const objPage = await validator.vMobileMGetListAll({
-      number: req.query.page ? parseInt(req.query.page.number) : 1,
-      size: req.query.page ? parseInt(req.query.page.size) : 100,
-    });
+    // const objPage = await validator.vMobileMGetListAll({
+    //   number: req.query.page ? parseInt(req.query.page.number) : 1,
+    //   size: req.query.page ? parseInt(req.query.page.size) : 100,
+    // });
 
-    const attentionLInDb = await db.Security.findAndCountAll({
+    // const attentionLInDb = await db.Security.findAndCountAll({
+    //   where: { active: true },
+    //   attributes: ["name", "phone", "address"],
+    //   limit: objPage.size,
+    //   offset: (objPage.number - 1) * objPage.size,
+    //   // Ordered from A-Z
+    //   order: [["name", "ASC"]],
+    // });
+
+    // const attentionLInDb = await db.Security.findAndCountAll({
+    const attentionLInDb = await db.Security.findAll({
       where: { active: true },
-      attributes: ["name", "phone", "address"],
-      limit: objPage.size,
-      offset: (objPage.number - 1) * objPage.size,
+      attributes: ["name", "phone", "address", "imageUri", "siteUri"],
+      // limit: objPage.size,
+      // offset: (objPage.number - 1) * objPage.size,
       // Ordered from A-Z
       order: [["name", "ASC"]],
     });
 
-    if (attentionLInDb.count <= 0)
+    if (!attentionLInDb || attentionLInDb.length === 0) {
       throw {
         status: StatusCodes.NOT_FOUND,
         message: "There are no Attention Lines registered in the database",
       };
-    if (attentionLInDb.rows.length <= 0)
+    }
+
+    const categInDb = await db.SecurityCategory.findAll({
+      attributes: ["id", "name", "imageUri", "siteUri", "color"],
+      // limit: objPage.size,
+      // offset: (objPage.number - 1) * objPage.size,
+      // Ordered from A-Z
+      order: [["name", "ASC"]],
+    });
+
+    if (!categInDb || categInDb.length === 0) {
       throw {
-        status: StatusCodes.BAD_REQUEST,
-        message: '"page.number" is too large for the number of possible pages',
+        status: StatusCodes.NOT_FOUND,
+        message: "There are no Attention Lines registered in the database",
       };
+    }
+
+    // if (attentionLInDb.count <= 0)
+    //   throw {
+    //     status: StatusCodes.NOT_FOUND,
+    //     message: "There are no Attention Lines registered in the database",
+    //   };
+    // if (attentionLInDb.rows.length <= 0)
+    //   throw {
+    //     status: StatusCodes.BAD_REQUEST,
+    //     message: '"page.number" is too large for the number of possible pages',
+    //   };
     // const totalPages = Math.ceil(attentionLInDb.count / objPage.size);
 
-    return res.status(StatusCodes.OK).send(attentionLInDb.rows);
+    const responseCustom = {
+      reportCategories: categInDb,
+      securityLines: attentionLInDb,
+    };
+
+    return res.status(StatusCodes.OK).send(responseCustom);
   } catch (error) {
-    console.error("attention lines could not be recovered: ", error.message);
+    // console.error("attention lines could not be recovered: ", error.message);
     return next(error);
   }
 };
