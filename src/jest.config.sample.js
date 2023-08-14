be613303aad182e20c44ec758f7d5d7f6a0f31f7
bitjep <julian.plazas@thebitbang.company>
2023-08-14 17:33:39 -0500

    [TEST] Added config details in jest.config.sample.js.

--- src/jest.config.sample.js
+++ src/jest.config.sample.js
@@ -1,15 +1,26 @@
 // Save your configuration as jest.config.js
 const config = {
+    // Test folder or files to ignore
+    testPathIgnorePatterns: ['utils'],  // Add the test-file names or -folder names that you want to ignore.
+    // Global jest constants
     globals: {
         notificationsMicroserviceLocalHost: 'http://localhost:3000',
         notificationsMicroserviceOnlineHost: 'https://k7gmmdc9dj.us-east-1.awsapprunner.com',
+        notificationsMicroserviceDefaultHost: 'http://localhost:3000',  // Replace with the local or online host
         usersMicroserviceLocalHost: 'http://localhost:3001',
         usersMicroserviceOnlineHost: 'https://vbxb7pp27j.us-east-1.awsapprunner.com',
-        firebaseKey: 'firebaseAccessKey',   // MUST Change
-        firebaseTestUserLogin: {
+        usersMicroserviceDefaultHost: 'http://localhost:3001',          // Replace with the local or online host
+        firebaseKey: 'firebaseAccessKey',           // MUST Change
+        firebaseTestMobileUserLogin: {
             returnSecureToken: true,
-            email: "test@testmail.com",     // MUST Change
-            password: "123456",             // MUST Change
+            email: "testMobile@testmail.com",       // MUST Change
+            password: "123456",                     // MUST Change
+            clientType: "CLIENT_TYPE_WEB",
+        },
+        firebaseTestWebUserLogin: {
+            returnSecureToken: true,
+            email: "testWeb@testmail.com",          // MUST Change
+            password: "123456",                     // MUST Change
             clientType: "CLIENT_TYPE_WEB",
         },
     },
