d5881dfc36ae7251f1fefe74e08c36d4ec41e8e3
Iván López <navis@unicauca.edu.co>
2023-08-14 11:49:08 -0500

    [Feat]: Get Attention Line Tests added.

---
+++ src/__tests__/notifications/mobileAttentionLines.js
@@ -0,0 +1,45 @@
+const request = require("supertest");
+
+const usedHost = `${global.notificationsMicroserviceLocalHost}/mobile/v1/notifications/attention_lines`;
+describe("Mobile - Attention Lines management API points: ", () => {
+  jest.setTimeout(8000);
+
+  const requestHeaders = {
+    Authorization: "Bearer ",
+  };
+
+  beforeAll(async () => {
+    const firebaseAuth = await request(
+      "https://identitytoolkit.googleapis.com/v1"
+    )
+      .post("/accounts:signInWithPassword")
+      .query({ key: global.firebaseKey })
+      .send(global.firebaseTestUserLogin);
+    requestHeaders.Authorization += firebaseAuth.body.idToken;
+  });
+
+  describe("GET /attention_lines/ ", () => {
+    test("should respond with status 200 and an object containing the phone and whatsapp properties.", async () => {
+      // No pagination
+      const response0 = await request(usedHost).get("/").set(requestHeaders);
+      expect(response0.statusCode).toBe(200);
+      expect(response0.body).toHaveProperty("phone");
+      expect(response0.body).toHaveProperty("whatsapp");
+    });
+
+    // {
+    //     "status": 401,
+    //     "detail": "Decoding Firebase ID token failed. Make sure you passed the entire string JWT which represents an ID token. See https://firebase.google.com/docs/auth/admin/verify-id-tokens for details on how to retrieve an ID token.",
+    //     "code": "Unauthorized"
+    // }
+    test("should fail with error 401 and a message if Authorization header is not set.", async () => {
+      const response0 = await request(usedHost).get("/");
+      expect(response0.statusCode).toBe(401);
+      expect(response0.body).not.toHaveProperty("meta");
+      expect(response0.body).not.toHaveProperty("data");
+      expect(response0.body).toHaveProperty("status", 401);
+      expect(response0.body).toHaveProperty("code");
+      expect(response0.body).toHaveProperty("detail");
+    });
+  });
+});
