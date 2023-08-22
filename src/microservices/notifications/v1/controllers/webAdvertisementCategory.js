5d661367a12ee94a74af28173f695e3fdac5a6c2
Daniel Collazos <daniel.collazos@thebitbang.company>
2023-08-22 12:53:08 -0500

    [FEAT]: Get all categories for advertising

--- src/microservices/notifications/v1/controllers/webAdvertisementCategory.js
+++ src/microservices/notifications/v1/controllers/webAdvertisementCategory.js
@@ -5,50 +5,19 @@ const validator = require('../../utils/validator');
 // Retrieve all the advertisement categories.
 const getAllCategories = async (req, res, next) => {
     try {
-        // TODO: implement list all
-        const { page: pagination } = await validator.validateSimplePaginationSchema(req.query);
-        const offset = (pagination.number - 1) * pagination.size;
-        // const pageAdvertisements = await db.Advertisement.findAndCountAll({
-        //     unique: true,
-        //     paranoid: true,
-        //     order: [["createdAt", "DESC"]],
-        //     offset,
-        //     limit: pagination.size,
-        //     include: [{
-        //         model: db.AdvertisementCategory,
-        //         attributes: ['name', 'color'],
-        //         required: false,
-        //     }],
-        //     attributes: {
-        //         exclude: ["deletedAt", "AdvertisementCategory"],
-        //         include: [
-        //             [Sequelize.col('"AdvertisementCategory"."name"'), 'categoryName'],
-        //             [Sequelize.col('"AdvertisementCategory"."color"'), 'categoryColor']
-        //         ],
-        //     },
-        // });
-        // if (pageAdvertisements.count <= 0)
-        //     throw {
-        //         status: StatusCodes.NOT_FOUND,
-        //         message: 'There are no Advertisements registered in the database.',
-        //     };
-        // if (pageAdvertisements.rows.length <= 0)
-        //     throw {
-        //         status: StatusCodes.BAD_REQUEST,
-        //         message: '"page.number" is too large for the number of possible pages.',
-        //     };
-        // const data = pageAdvertisements.rows.map(row => {
-        //     return { ...row.dataValues, AdvertisementCategory: undefined };
-        // });
+        const pageAdvertisements = await db.AdvertisementCategory.findAll({
+            unique: true,
+            paranoid: true,
+            order: [["createdAt", "DESC"]],
+            attributes: {
+                exclude: ["deletedAt", "AdvertisementCategory"],
+                include: ["id", "name", "color"],
+            },
+        });
         return res.status(StatusCodes.OK).json({
-            message: 'TODO: implement list all'
-            // meta: {
-            //     page: pagination.number,
-            //     pageSize: pagination.size,
-            //     totalRecords: pageAdvertisements.count,
-            //     totalPages: Math.ceil(pageAdvertisements.count / pagination.size),
-            // },
-            // data,
+            message: 'TODO: implement list all',
+            meta: {},
+            data: pageAdvertisements,
         });
     } catch (error) {
         return next(error);
