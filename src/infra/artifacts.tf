3a99b17c07fc14c4062a60166096b1093ce23a6f
Rafael Ceron Espinosa <rafaelceronespinosa@Rafaels-MacBook-Pro-2.local>
2023-08-30 17:25:13 -0500

    [INFRA] artifacts creation with terraform

---
+++ src/infra/artifacts.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+variable "project_id" {
+  type    = string
+  default = "cali-mobility"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east1"
+}
+
+variable "zone" {
+  type    = string
+  default = "us-east1-c"
+}
+
+
+provider "google" {
+  credentials = file("cali-mobility-sa.json")
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+}
+
+resource "google_artifact_registry_repository" "artifactory_repository" {
+  project       = var.project_id
+  location      = var.region
+  repository_id = "cali-mobility-third-party"
+  format        = "DOCKER"
+  description   = "cali-mobility-third-party repository "
+}
