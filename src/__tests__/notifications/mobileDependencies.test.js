12ed01912833e20c4a531de3ea1f5744771b7efe
bitjep <julian.plazas@thebitbang.company>
2023-08-31 17:37:25 -0500

    [TEST] Defined test for mobile - GetDependencies.

---
+++ src/__tests__/notifications/mobileDependencies.test.js
@@ -0,0 +1,122 @@
+const request = require('supertest');
+
+const usedHost = `${global.notificationsMicroserviceDefaultHost}/mobile/v1/notifications/attention_lines/dependencies`;
+
+describe("Dependencies consumption API points: ", () => {
+    jest.setTimeout(8000);
+
+    const requestHeaders = {
+        Authorization: "Bearer ",
+    };
+
+    const dependenciesItem = {
+        id: expect.any(Number),
+        name: expect.any(String),
+    };
+
+    beforeAll(async () => {
+        const firebaseAuth = await request("https://identitytoolkit.googleapis.com/v1")
+            .post('/accounts:signInWithPassword')
+            .query({ key: global.firebaseKey })
+            .send(global.firebaseTestMobileUserLogin);
+        requestHeaders.Authorization += firebaseAuth.body.idToken;
+        // console.log(requestHeaders);
+    });
+
+    describe("GET /notifications/attention_lines/dependencies ", () => {
+        test("should respond with status 200 and an array of objects with: id and name.", async () => {
+            const response0 = await request(usedHost).get('/').set(requestHeaders);
+            expect(response0.statusCode).toBe(200);
+            expect(response0.body).toEqual(expect.any(Array));
+            expect(response0.body.length).toBeGreaterThanOrEqual(0);
+            response0.body.forEach(item => {
+                expect(item).toMatchSnapshot(dependenciesItem);
+            });
+        });
+
+        test("should respond with status 200 and a shorter array of objects with: id and name if request is paginated.", async () => {
+            const response0 = await request(usedHost).get('/').set(requestHeaders)
+                .query({ page: { number: 1, size: 2 } });
+            expect(response0.statusCode).toBe(200);
+            expect(response0.body).toEqual(expect.any(Array));
+            expect(response0.body.length).toBe(2);
+            response0.body.forEach(item => {
+                expect(item).toMatchSnapshot(dependenciesItem);
+            });
+        });
+
+        test("should respond with error 400 if pagination is wrongly provided", async () => {
+            const response1 = await request(usedHost).get('/')
+                .set(requestHeaders)
+                .query({ page: null });
+            expect(response1.statusCode).toBe(400);
+            expect(response1.body).not.toHaveProperty("data");
+            expect(response1.body).toHaveProperty("status", 400);
+            expect(response1.body).toHaveProperty("code");
+            expect(response1.body).toHaveProperty("detail");
+
+            const response2 = await request(usedHost).get('/')
+                .set(requestHeaders)
+                .query({ page: { number: 1 } });
+            expect(response2.statusCode).toBe(400);
+            expect(response2.body).not.toHaveProperty("data");
+            expect(response2.body).toHaveProperty("status", 400);
+            expect(response2.body).toHaveProperty("code");
+            expect(response2.body).toHaveProperty("detail");
+
+            const response3 = await request(usedHost).get('/')
+                .set(requestHeaders)
+                .query({ page: { size: 1 } });
+            expect(response3.statusCode).toBe(400);
+            expect(response3.body).not.toHaveProperty("data");
+            expect(response3.body).toHaveProperty("status", 400);
+            expect(response3.body).toHaveProperty("code");
+            expect(response3.body).toHaveProperty("detail");
+
+            const response4 = await request(usedHost).get('/')
+                .set(requestHeaders)
+                .query({ page: { number: 0, size: 1 } });
+            expect(response4.statusCode).toBe(400);
+            expect(response4.body).not.toHaveProperty("data");
+            expect(response4.body).toHaveProperty("status", 400);
+            expect(response4.body).toHaveProperty("code");
+            expect(response4.body).toHaveProperty("detail");
+
+            const response5 = await request(usedHost).get('/')
+                .set(requestHeaders)
+                .query({ page: { number: 1, size: 0 } });
+            expect(response5.statusCode).toBe(400);
+            expect(response5.body).not.toHaveProperty("data");
+            expect(response5.body).toHaveProperty("status", 400);
+            expect(response5.body).toHaveProperty("code");
+            expect(response5.body).toHaveProperty("detail");
+
+            const response6 = await request(usedHost).get('/')
+                .set(requestHeaders)
+                .query({ page: { number: "A", size: 2 } });
+            expect(response6.statusCode).toBe(400);
+            expect(response6.body).not.toHaveProperty("data");
+            expect(response6.body).toHaveProperty("status", 400);
+            expect(response6.body).toHaveProperty("code");
+            expect(response6.body).toHaveProperty("detail");
+
+            const response7 = await request(usedHost).get('/')
+                .set(requestHeaders)
+                .query({ page: { number: 2, size: "B" } });
+            expect(response7.statusCode).toBe(400);
+            expect(response7.body).not.toHaveProperty("data");
+            expect(response7.body).toHaveProperty("status", 400);
+            expect(response7.body).toHaveProperty("code");
+            expect(response7.body).toHaveProperty("detail");
+        });
+
+        test("should fail with error 401 and a message if Authorization header is not set.", async () => {
+            const response0 = await request(usedHost).get('/');
+            expect(response0.statusCode).toBe(401);
+            expect(response0.body).not.toHaveProperty("data");
+            expect(response0.body).toHaveProperty("status", 401);
+            expect(response0.body).toHaveProperty("code");
+            expect(response0.body).toHaveProperty("detail");
+        });
+    });
+});
