02999e16f4e36f074de508685bf878494a1b2888
Rafael Ceron Espinosa <rafaelceronespinosa@Rafaels-MacBook-Pro-2.local>
2023-09-05 16:31:47 -0500

    [FEATURE] include more than one service in our deployment

--- src/infra/artifacts.tf
+++ src/infra/artifacts.tf
@@ -33,7 +33,6 @@ variable "zone" {
   default = "us-east1-c"
 }
 
-
 provider "google" {
   credentials = file("cali-mobility-master-key.json")
   project     = var.project_id
@@ -41,6 +40,7 @@ provider "google" {
   zone        = var.zone
 }
 
+/*
 resource "google_artifact_registry_repository" "artifactory_repository" {
   project      = var.project_id
   location      = var.region
@@ -49,54 +49,139 @@ resource "google_artifact_registry_repository" "artifactory_repository" {
   description   = "cali-mobility-third-party repository "
 }
 
-resource "google_cloud_run_service" "default" {
+resource "google_artifact_registry_repository" "artifactory_repository" {
+  project      = var.project_id
+  location      = var.region
+  repository_id = "cali-mobility-admin"
+  format        = "DOCKER"
+  description   = "cali-mobility-admin repository "
+}
+
+resource "google_artifact_registry_repository" "artifactory_repository" {
+  project      = var.project_id
+  location      = var.region
+  repository_id = "cali-mobility-file-management"
+  format        = "DOCKER"
+  description   = "cali-mobility-file-management repository "
+}
+
+resource "google_artifact_registry_repository" "artifactory_repository" {
+  project      = var.project_id
+  location      = var.region
+  repository_id = "cali-mobility-notifications"
+  format        = "DOCKER"
+  description   = "cali-mobility-notifications repository "
+}
+
+resource "google_artifact_registry_repository" "artifactory_repository" {
+  project      = var.project_id
+  location      = var.region
+  repository_id = "cali-mobility-users"
+  format        = "DOCKER"
+  description   = "cali-mobility-users repository "
+}
+
+resource "google_artifact_registry_repository" "artifactory_repository" {
+  project      = var.project_id
+  location      = var.region
+  repository_id = "cali-mobility-frontend"
+  format        = "DOCKER"
+  description   = "cali-mobility-frontend repository "
+}
+*/
+resource "google_cloud_run_v2_service" "third-parties" {
   name     = "third-parties"
   location = var.service_region
   template {
-    metadata {
-      annotations = {
-        "autoscaling.knative.dev/minScale" = "0"
-        "autoscaling.knative.dev/maxScale" = "1"
-      }
+    scaling {
+      max_instance_count = 1
+      min_instance_count = 0
     }
-    spec {
-      containers {
-        image = "us-east1-docker.pkg.dev/cali-mobility/cali-mobility-notifications/notifications:latest"
-        ports {
-          container_port = 3000
-        }
-        env {
-          name = "BUCKET"
-          value = "cali-mobility-data"
-        }
-        env {
-          name = "FCM_TOPIC_NAME_MOBILE"
-          value = "mobileUsersNotifications"
-        }
-        env {
-          name = "TWILIO_ACCOUNT_SID"
-          value = "ACyourtwilioaccountsid"
-        }
-        env {
-          name = "TWILIO_AUTH_TOKEN"
-          value = "yourtwilioauthenticationtoken"
-        }
-        env {
-          name = "TWILIO_MESSAGE_SERVICE_SID"
-          value = "yourtwilioMessageServiceSID"
-        }
-        liveness_probe {
-          http_get {
-            path = "/health"
-          }
+    execution_environment = "EXECUTION_ENVIRONMENT_GEN2"
+    containers {
+      image = "us-east1-docker.pkg.dev/cali-mobility/cali-mobility-notifications/notifications:latest"
+      ports {
+        container_port = 3000
+      }
+      env {
+        name = "BUCKET"
+        value = "cali-mobility-data"
+      }
+      env {
+        name = "FCM_TOPIC_NAME_MOBILE"
+        value = "mobileUsersNotifications"
+      }
+      env {
+        name = "TWILIO_ACCOUNT_SID"
+        value = "ACyourtwilioaccountsid"
+      }
+      env {
+        name = "TWILIO_AUTH_TOKEN"
+        value = "yourtwilioauthenticationtoken"
+      }
+      env {
+        name = "TWILIO_MESSAGE_SERVICE_SID"
+        value = "yourtwilioMessageServiceSID"
+      }
+      liveness_probe {
+        http_get {
+          path = "/health"
         }
       }
     }
   }
 
   traffic {
+    type = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
     percent = 100
-    latest_revision = true
+  }
+}
+
+resource "google_cloud_run_v2_service" "users" {
+  name     = "users"
+  location = var.service_region
+  template {
+    scaling {
+      max_instance_count = 1
+      min_instance_count = 0
+    }
+    execution_environment = "EXECUTION_ENVIRONMENT_GEN2"
+    containers {
+      image = "us-east1-docker.pkg.dev/cali-mobility/cali-mobility-users/users:latest"
+      ports {
+        container_port = 3000
+      }
+      env {
+        name = "BUCKET"
+        value = "cali-mobility-data"
+      }
+      env {
+        name = "FCM_TOPIC_NAME_MOBILE"
+        value = "mobileUsersNotifications"
+      }
+      env {
+        name = "TWILIO_ACCOUNT_SID"
+        value = "ACyourtwilioaccountsid"
+      }
+      env {
+        name = "TWILIO_AUTH_TOKEN"
+        value = "yourtwilioauthenticationtoken"
+      }
+      env {
+        name = "TWILIO_MESSAGE_SERVICE_SID"
+        value = "yourtwilioMessageServiceSID"
+      }
+      liveness_probe {
+        http_get {
+          path = "/health"
+        }
+      }
+    }
+  }
+
+  traffic {
+    type = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+    percent = 100
   }
 }
 
@@ -109,37 +194,17 @@ data "google_iam_policy" "noauth" {
   }
 }
 
-resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.default.location
-  project     = google_cloud_run_service.default.project
-  service     = google_cloud_run_service.default.name
+resource "google_cloud_run_service_iam_policy" "noauth-users" {
+  location    = google_cloud_run_v2_service.users.location
+  project     = google_cloud_run_v2_service.users.project
+  service     = google_cloud_run_v2_service.users.name
   policy_data = data.google_iam_policy.noauth.policy_data
 }
 
-// cloud run configuration
-/*
-module "cloud_run" {
-  source  = "GoogleCloudPlatform/cloud-run/google"
-  version = "~> 0.2.0"
-  # Required variables
-  service_name           = ""
-  project_id             = var.project_id
-  location               = var.service_region
-  image                  = ""
-  service_account_email = "85094745134-compute@developer.gserviceaccount.com"
-  ports = {
-    "name": "open port",
-    "port": 3000
-  }
-  env_vars = [
-    {
-      "value": "cali-mobility-data",
-      "name": "BUCKET"
-    }
-  ]
-  template_annotations = {
-    "autoscaling.knative.dev/maxScale": 2,
-    "autoscaling.knative.dev/minScale": 0,
-  }
+resource "google_cloud_run_service_iam_policy" "noauth-third-parties" {
+  location    = google_cloud_run_v2_service.third-parties.location
+  project     = google_cloud_run_v2_service.third-parties.project
+  service     = google_cloud_run_v2_service.third-parties.name
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
-*/
+
