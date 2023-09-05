b507af21425c88d1c43b0b54ba1d4c5445d77d90
AndresGarzonJ-TBBC <andres.garzon@thebitbang.company>
2023-09-05 16:16:17 -0500

    [Feat] webTransportCompanies - Add get all enpoint

--- src/microservices/thirdParties/v1/controllers/web/transportCompanies.js
+++ src/microservices/thirdParties/v1/controllers/web/transportCompanies.js
@@ -241,3 +241,67 @@ exports.postDelete = async (req, res, next) => {
     return next(error);
   }
 };
+
+/**
+ * Get all transport companies 
+ * @param {object} req.query - Object containing the number and size
+ * @return {object} Response contains: statuscode (integer), json (objeto): data transport companies. Or if there's error, json (objeto): status, code, detail
+ */
+exports.getAll = async (req, res, next) => {
+  try {
+    // // ! Pendiente: Validar permisos del usuario
+    // const createdBy = await db.User.findOne({
+    //   where: { disabled: false, userMobile: false, clientId: res.locals.uid },
+    //   attributes: ["id"],
+    // });
+
+    // if (createdBy == null || createdBy.id == null)
+    //   throw {
+    //     message: "User not found",
+    //     status: StatusCodes.NOT_FOUND,
+    //   };
+
+    const objPage = await validator.vWebGetListAll({
+      number: req.query.page ? parseInt(req.query.page.number) : null,
+      size: req.query.page ? parseInt(req.query.page.size) : null,
+    });
+
+    const companiesInDb = await db.TransportCompany.findAndCountAll({
+      // // ! Pendiente: Validar permisos del usuario
+      // where: { createdBy: createdBy.id },
+      limit: objPage.size,
+      offset: (objPage.number - 1) * objPage.size,
+      order: [["createdAt", "DESC"]], // Sort by date of creation in descending order
+      attributes: {
+        exclude: ["createdBy", "geolocation", "deletedAt"],
+      },
+    });
+
+    if (companiesInDb.count <= 0)
+      throw {
+        status: StatusCodes.NOT_FOUND,
+        message: "There are not transport companies registered",
+      };
+    if (companiesInDb.rows.length <= 0)
+      throw {
+        status: StatusCodes.BAD_REQUEST,
+        message: '"page.number" is too large for the number of possible pages',
+      };
+    const totalPages = Math.ceil(companiesInDb.count / objPage.size);
+
+    const responseCustom = {
+      meta: {
+        page: objPage.number,
+        pageSize: objPage.size,
+        totalRecords: companiesInDb.count,
+        totalPages: totalPages,
+      },
+      data: companiesInDb.rows,
+    };
+
+    return res.status(StatusCodes.OK).send(responseCustom);
+  } catch (error) {
+    // console.error("Transport companies could not be recovered: ", error.message);
+    return next(error);
+  }
+};
