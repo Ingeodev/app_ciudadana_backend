a23999198c444f00c8e32f566f3571399aacac92
bitjep <julian.plazas@thebitbang.company>
2023-09-07 17:33:21 -0500

    [FEATURE] Added template services for webSecurityAttentionPoints.

---
+++ src/microservices/notifications/v1/controllers/webSecurityAttentionPoint.js
@@ -0,0 +1,87 @@
+const { StatusCodes } = require('http-status-codes');
+
+const db = require('../../../../models');
+const validator = require('../../utils/validator');
+
+// Create an security attention point.
+const postCreateSecurityAttentionPoint = async (req, res, next) => {
+    try {
+        return res.status(StatusCodes.CREATED)
+            .json({
+                data: { msg: 'TODO - Create a security attention point.' },
+            });
+    } catch (error) {
+        return next(error);
+    }
+};
+
+// Edit an security attention point.
+const postEditSecurityAttentionPoint = async (req, res, next) => {
+    try {
+        return res.status(StatusCodes.CREATED)
+            .json({
+                data: { msg: 'TODO - Edit a security attention point.' },
+            });
+    } catch (error) {
+        return next(error);
+    }
+};
+
+// Delete an security attention point.
+const postDeleteSecurityAttentionPoint = async (req, res, next) => {
+    try {
+        return res.status(StatusCodes.CREATED)
+            .json({
+                data: { msg: 'TODO - Delete a security attention point.' },
+            });
+    } catch (error) {
+        return next(error);
+    }
+};
+
+// Retrieve all the available security attention points.
+const getAllSecurityAttentionPoints = async (req, res, next) => {
+    try {
+        const { page: pagination } = await validator.validateSimplePaginationSchema(req.query);
+        const offset = (pagination.number - 1) * pagination.size;
+        const pagePoints = await db.SecurityAttentionPoint.findAndCountAll({
+            unique: true,
+            paranoid: true,
+            order: [["createdAt", "DESC"]],
+            offset,
+            limit: pagination.size,
+            attributes: {
+                exclude: ["deletedAt"],
+            },
+        });
+        if (pagePoints.count <= 0)
+            throw {
+                status: StatusCodes.NOT_FOUND,
+                message: 'There are no Security Attention Points registered in the database.',
+            };
+        if (pagePoints.rows.length <= 0)
+            throw {
+                status: StatusCodes.BAD_REQUEST,
+                message: '"page[number]" is too large for the number of possible pages.',
+            };
+        const data = pagePoints.rows.map(row => row.dataValues);
+        return res.status(StatusCodes.OK).json({
+            meta: {
+                page: pagination.number,
+                pageSize: pagination.size,
+                totalRecords: pagePoints.count,
+                totalPages: Math.ceil(pagePoints.count / pagination.size),
+            },
+            data,
+        });
+    } catch (error) {
+        return next(error);
+    }
+};
+
+module.exports = {
+    postCreateSecurityAttentionPoint,
+    postEditSecurityAttentionPoint,
+    postDeleteSecurityAttentionPoint,
+    getAllSecurityAttentionPoints,
+};
