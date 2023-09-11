f6aa54da54fe1dcb69e61279d36a8724de2d7dfe
AndresGarzonJ-TBBC <andres.garzon@thebitbang.company>
2023-09-11 10:44:31 -0500

    [Feat] WebCity - Create table in db

---
+++ src/migrations/20230907022757-create-cities.js
@@ -0,0 +1,60 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      "Cities",
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          unique: true,
+        },
+        city: {
+          type: Sequelize.STRING(50),
+          allowNull: false,
+          unique: false,
+        },
+        cityCode: {
+          type: Sequelize.STRING(50),
+          allowNull: true,
+          unique: false,
+        },
+        state: {
+          type: Sequelize.STRING(50),
+          allowNull: true,
+          unique: false,
+        },
+        createdAt: {
+          type: "TIMESTAMP",
+          allowNull: false,
+        },
+        updatedAt: {
+          type: "TIMESTAMP",
+          allowNull: true,
+        },
+        deletedAt: {
+          type: "TIMESTAMP",
+          allowNull: true,
+        },
+      },
+      {
+        tableName: "Cities",
+        schema: "public",
+      }
+    );
+    return await queryInterface.sequelize.query(`
+      CREATE UNIQUE INDEX idx_unique_city_state
+      ON "Cities"("city", "state")
+      WHERE "deletedAt" IS NULL;
+    `);
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      DROP INDEX IF EXISTS idx_unique_city_state;
+    `);
+    await queryInterface.dropTable("Cities");
+  },
+};
---
+++ src/models/city.js
@@ -0,0 +1,63 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class City extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of DataTypes lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      City.hasMany(models.TransportRoute, {
+        foreignKey: {
+          name: "origin",
+          allowNull: false,
+          unique: false,
+        },
+      });
+      City.hasMany(models.TransportRoute, {
+        foreignKey: {
+          name: "destination",
+          allowNull: false,
+          unique: false,
+        },
+      });
+    }
+  }
+  City.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true,
+      },
+      city: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: false,
+      },
+      cityCode: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: false,
+      },
+      state: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "City",
+      tableName: "Cities",
+      schema: "public",
+      paranoid: true,
+      timestamps: true,
+    }
+  );
+  return City;
+};
