271947ecc0fecb89620bbd11961b97eac90abd0b
AndresGarzonJ-TBBC <andres.garzon@thebitbang.company>
2023-09-13 03:30:30 -0500

    [Fix] WebRouteTimetables - add idx_unique_date_routeId

--- src/migrations/20230907292798-create-route-timetables.js
+++ src/migrations/20230907292798-create-route-timetables.js
@@ -48,6 +48,11 @@ module.exports = {
         schema: "public",
       }
     );
+    await queryInterface.sequelize.query(`
+      CREATE UNIQUE INDEX idx_unique_date_routeId
+      ON "RouteTimetables"("date", "routeId")
+      WHERE "deletedAt" IS NULL;
+    `);
     return await queryInterface.addConstraint("RouteTimetables", {
       name: "fk_RouteTimetables_Route",
       fields: ["routeId"],
@@ -62,6 +67,9 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.removeConstraint("RouteTimetables", "fk_RouteTimetables_Route");
+    await queryInterface.sequelize.query(`
+      DROP INDEX IF EXISTS idx_unique_date_routeId;
+    `);
     await queryInterface.dropTable("RouteTimetables");
   },
 };
