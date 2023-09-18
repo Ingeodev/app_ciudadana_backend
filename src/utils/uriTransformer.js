e324e57ee5dd976ceb33f2cc492fb114db25322d
bitjep <julian.plazas@thebitbang.company>
2023-09-18 10:46:15 -0500

    [FEATURE] Added function to modify files URIs.

---
+++ src/utils/uriTransformer.js
@@ -0,0 +1,36 @@
+/**
+ * Default keyword that is used to save file paths in the database.
+ */
+const filesMsHostUriReplaceKeyword = "|__this host__|";
+
+/**
+ * Default base URI of the FILE MANAGEMENT MICROSERVICE.
+ */
+const filesMsHostUri = process.env.FILES_MS_HOST_BASE_URI;
+
+/**
+ * Function that replaces the default FILE MANAGEMENT MICROSERVICE base URI into a default keyword that will later be used to replace.
+ * @param {String} uri File or Image URI as received from the request.
+ * @returns The `imageUri` or `fileUri` that should be saved in the DB if the image is saved in the File Management Microservice
+ */
+const transformReceivedUriToSave = (uri) => {
+    const transformedURI = uri.replace(filesMsHostUri, filesMsHostUriReplaceKeyword);
+    return transformedURI;
+};
+
+/**
+ * Function that replaces the default keyword for the default base URI of the FILE MANAGEMENT MICROSERVICE so files can be downloaded.
+ * @param {String} uri File or Image URI as received form the database.
+ * @returns The `imageUri` or `fileUri` that allows downloading and should be returned to users.
+ */
+const transformSavedUriToSend = (uri) => {
+    const transformedURI = uri.replace(filesMsHostUriReplaceKeyword, filesMsHostUri);
+    return transformedURI;
+};
+
+module.exports = {
+    filesMsHostUri,
+    filesMsHostUriReplaceKeyword,
+    transformReceivedUriToSave,
+    transformSavedUriToSend,
+};
