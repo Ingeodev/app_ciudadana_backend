14a480bc48cf2a9d09ebc49f1641486edfd48431
bitjep <julian.plazas@thebitbang.company>
2023-09-27 10:01:54 -0500

    [FEATURE] Added mobile list all Tourism Categories.

--- src/microservices/thirdParties/v1/controllers/mobile/tourismCategories.js
+++ src/microservices/thirdParties/v1/controllers/mobile/tourismCategories.js
@@ -7,7 +7,32 @@ const { formatColorOutputForMobile } = require("../../../../../utils/mobileColor
 /** List all the tourism categories in the mobile format */
 const getAll = async (req, res, next) => {
   try {
-    return res.status(StatusCodes.OK).send({ meta: { msg: 'TODO: Implement' } });
+    const { page } = await validator.validateOptionalPaginationSchema(req.query);
+    const pagination = {
+      number: 1,
+      size: 500,
+      ...page,
+    };
+    const offset = (pagination.number - 1) * pagination.size;
+    const pageTourCats = await db.TourismCategory.findAll({
+      paranoid: true,
+      order: [["createdAt", "DESC"]],
+      offset,
+      limit: pagination.size,
+      attributes: ['id', 'name', 'color', 'icon', 'iconMap'],
+    });
+
+    const data = pageTourCats.map(row => {
+      const returnData = {
+        id: row.dataValues.id,
+        name: row.dataValues.name,
+        color: formatColorOutputForMobile(row.dataValues.color),
+        icon: row.dataValues.icon,
+        iconMap: row.dataValues.iconMap,
+      }
+      return returnData;
+    });
+    return res.status(StatusCodes.OK).json(data);
   } catch (error) {
     return next(error);
   }
