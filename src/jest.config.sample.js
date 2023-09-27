4b2483c17f6265f20ce83f8542d7c8d201fefbf4
bitjep <julian.plazas@thebitbang.company>
2023-09-27 14:27:28 -0500

    [FEATURE] Added online endpoints for all microservices in example jest config.

--- src/jest.config.sample.js
+++ src/jest.config.sample.js
@@ -7,11 +7,20 @@ const config = {
     // Global jest constants
     globals: {
         notificationsMicroserviceLocalHost: 'http://localhost:3000',
-        notificationsMicroserviceOnlineHost: 'https://k7gmmdc9dj.us-east-1.awsapprunner.com',
+        notificationsMicroserviceOnlineHost: 'https://notifications-cmiesjcqoq-uc.a.run.app',
         notificationsMicroserviceDefaultHost: 'http://localhost:3000',  // Replace with the local or online host
         usersMicroserviceLocalHost: 'http://localhost:3001',
-        usersMicroserviceOnlineHost: 'https://vbxb7pp27j.us-east-1.awsapprunner.com',
+        usersMicroserviceOnlineHost: 'https://users-cmiesjcqoq-uc.a.run.app',
         usersMicroserviceDefaultHost: 'http://localhost:3001',          // Replace with the local or online host
+        adminMicroserviceLocalHost: 'http://localhost:3002',
+        adminMicroserviceOnlineHost: 'https://admin-cmiesjcqoq-uc.a.run.app',
+        adminMicroserviceDefaultHost: 'http://localhost:3002',          // Replace with the local or online host
+        fileManagementMicroserviceLocalHost: 'http://localhost:3003',
+        fileManagementMicroserviceOnlineHost: 'https://file-management-cmiesjcqoq-uc.a.run.app',
+        fileManagementMicroserviceDefaultHost: 'http://localhost:3003',          // Replace with the local or online host
+        thirdPartiesMicroserviceLocalHost: 'http://localhost:3004',
+        thirdPartiesMicroserviceOnlineHost: 'https://third-parties-cmiesjcqoq-uc.a.run.app',
+        thirdPartiesMicroserviceDefaultHost: 'http://localhost:3004',          // Replace with the local or online host
         firebaseKey: 'firebaseAccessKey',           // MUST Change
         firebaseTestMobileUserLogin: {
             returnSecureToken: true,
