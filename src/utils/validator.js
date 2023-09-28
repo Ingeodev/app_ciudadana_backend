f01062d4df2b4b8cb219d4a562696ddfc8f340c9
bitjep <julian.plazas@thebitbang.company>
2023-09-28 15:38:17 -0500

    [FEATURE] Added custom general validator isInCaliCustomJoiValidator to check position.

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,4 +1,27 @@
-const {StatusCodes} = require("http-status-codes");
+const { StatusCodes } = require("http-status-codes");
+const { isLocationInCali } = require("./polygonCali");
+const Joi = require("joi");
+
+/**
+ * Joi Custom validator that checks whether the geo point is within the limits of Cali, Valle, Colombia.
+ * @param {*} data Validation object possibly containing the lat and lon keys. If both keys are not present then this function just skips.
+ * @param {Joi.CustomHelpers} helpers 
+ * @returns `data` on successful validation; Joi error with validation message on validation error.
+ */
+const isInCaliCustomJoiValidator = (data, helpers) => {
+  if (data.lon != null && data.lat != null)
+    if (!isLocationInCali(data.lat, data.lon))
+      return helpers.message('The combination of "lat" and "lon" must belong to the area of the municipality of Cali, Valle del Cauca, Colombia');
+  return data;
+};
+
+/**
+ * Asyncronously uses the `validator_schema` to validate the incoming `data` with Joi.
+ * @param {Joi.ObjectSchema} validator_schema Validation schema to use.
+ * @param {object} data Incoming data to validate.
+ * @returns {object} Validated data.
+ * @throws Validation error and BAD_REQUEST (400) status on validation failure.
+ */
 const use_validator_on_data = async (validator_schema, data) => {
   try {
     if (!validator_schema) {
@@ -18,5 +41,6 @@ const use_validator_on_data = async (validator_schema, data) => {
 };
 
 module.exports = {
-  use_validator_on_data
+  use_validator_on_data,
+  isInCaliCustomJoiValidator,
 }
