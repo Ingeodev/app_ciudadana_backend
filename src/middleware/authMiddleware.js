df467f133cffad4c42f20b263eaf6d2a8b3c4267
AndresGarzonJ-TBBC <andres.garzon@thebitbang.company>
2023-11-01 15:12:54 -0500

    [Fix] mobile: authMiddleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -67,50 +67,58 @@ const authMiddleware = async (req, res, next) => {
 const authMiddlewareMobile = async (req, res, next) => {
   try {
     const { authorization } = req.headers;
-    if (authorization) {
-      if (!authorization.startsWith("Bearer"))
-        throw {
-          status: StatusCodes.UNAUTHORIZED,
-          message: "An authorization token starting with 'Bearer' is expected",
-        };
-      const split = authorization.split("Bearer ");
-      if (split.length !== 2)
-        throw {
-          status: StatusCodes.UNAUTHORIZED,
-          message:
-            "Authorization header is not in the expected format. It should be 'Bearer [token]'",
-        };
-
-      const token = split[1];
-      const decodedToken = await appFirebase.auth().verifyIdToken(token);
-
-      // Verify that the user is not deleted
-      let userInDb = null;
-      if (decodedToken.user_id !== null) {
-        userInDb = await db.User.findOne({
-          where: {
-            clientId: decodedToken.user_id,
-            userMobile: true
-          },
-          attributes: ["id", "clientId", "deletedAt", "disabled"],
-          paranoid: false,
-        });
-      }
-
-      if (userInDb !==  null) {
-        if (userInDb.dataValues.deletedAt !== null || userInDb.dataValues.disabled === true)
-          throw {
-            message: "User not found.",
-            status: StatusCodes.UNAUTHORIZED,
-          };
-      }
-      res.locals = {
-        ...res.locals,
-        uid: decodedToken.user_id,
-        role: decodedToken.role,
-        // emailVerified: decodedToken.email_verified,
+    if (!authorization)
+      throw {
+        status: StatusCodes.UNAUTHORIZED,
+        message: "Missing header Authorization",
       };
+
+    if (!authorization.startsWith("Bearer"))
+      throw {
+        status: StatusCodes.UNAUTHORIZED,
+        message: "An authorization token starting with 'Bearer' is expected",
+      };
+    const split = authorization.split("Bearer ");
+    if (split.length !== 2)
+      throw {
+        status: StatusCodes.UNAUTHORIZED,
+        message:
+          "Authorization header is not in the expected format. It should be 'Bearer [token]'",
+      };
+
+    const token = split[1];
+    const decodedToken = await appFirebase.auth().verifyIdToken(token);
+
+    // Verify that the user is not deleted
+    let userInDb = null;
+    if (decodedToken.user_id !== null) {
+      userInDb = await db.User.findOne({
+        where: {
+          clientId: decodedToken.user_id,
+          userMobile: true,
+        },
+        attributes: ["id", "clientId", "deletedAt", "disabled"],
+        paranoid: false,
+      });
     }
+
+    if (userInDb !== null) {
+      if (
+        userInDb.dataValues.deletedAt !== null ||
+        userInDb.dataValues.disabled === true
+      )
+        throw {
+          message: "User not found.",
+          status: StatusCodes.UNAUTHORIZED,
+        };
+    }
+    res.locals = {
+      ...res.locals,
+      uid: decodedToken.user_id,
+      role: decodedToken.role,
+      // emailVerified: decodedToken.email_verified,
+    };
+
     return next();
   } catch (error) {
     if (error.code) error.status = StatusCodes.UNAUTHORIZED;
