ed178a6d28f75e08711e822338adc524f4653741
AndresGarzonJ-TBBC <andres.garzon@thebitbang.company>
2023-11-01 10:50:13 -0500

    [Refactor] mobile - getAttentionLine.

--- src/microservices/notifications/v1/controllers/mobileAttentionLines.js
+++ src/microservices/notifications/v1/controllers/mobileAttentionLines.js
@@ -1,10 +1,10 @@
 const { StatusCodes } = require("http-status-codes");
 const db = require("../../../../models/index.js");
-const validator = require("../../utils/validatorAttentionLines.js");
+// const validator = require("../../utils/validatorAttentionLines.js");
 
 /**
  * Get current attention line
- * @return {object} Response contains: statuscode (integer), json (objeto): data attention line. Or if there's error, json (objeto): status, code, detail
+ * @return {object} Response contains: statusCode (integer), json (objeto): data attention line. Or if there's error, json (objeto): status, code, detail
  */
 exports.getAttentionLine = async (req, res, next) => {
   try {
