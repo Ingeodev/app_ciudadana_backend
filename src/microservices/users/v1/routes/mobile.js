ffa160f708391b66f72de1e348d6000d6d09a9c1
Ingeodev <ingeodev@gmail.com>
2026-09-19 11:28:09 -0500

    fix(upload): remove .single() from remaining mobile routes

--- src/microservices/users/v1/routes/mobile.js
+++ src/microservices/users/v1/routes/mobile.js
@@ -16,7 +16,7 @@ router.post(
 router.post(
   "/account/full_login",
   // hasPermissions({ role: "super_master_user" }),
-  uploadImagesPdfs.single("file"),
+  uploadImagesPdfs,
   usersMobile.postAccountBaseLogin
 );
 
